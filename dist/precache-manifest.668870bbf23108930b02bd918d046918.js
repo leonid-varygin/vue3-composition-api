605e750221d020e0a4cfec615485b292db9782b7
Leonid Varygin <leonid.warygin@gmail.com>
2021-01-29 14:39:56 +0700

    deploy to firebase and dist folder created

---
+++ dist/precache-manifest.668870bbf23108930b02bd918d046918.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f816b3ed71ed1c929b13",
+    "url": "/css/app.37adbbe3.css"
+  },
+  {
+    "revision": "731d44e36acf5ee112556ced4973cd4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f816b3ed71ed1c929b13",
+    "url": "/js/app.8ca8ac7a.js"
+  },
+  {
+    "revision": "33a47609d2432ae73d3b",
+    "url": "/js/chunk-2d0a4783.a89176a8.js"
+  },
+  {
+    "revision": "1258428d6ac6e6416f29",
+    "url": "/js/chunk-2d0be70b.a50830a0.js"
+  },
+  {
+    "revision": "c80069a16b36b7579b28",
+    "url": "/js/chunk-2d216dc7.821ef3f4.js"
+  },
+  {
+    "revision": "bd7a4d8c4d4e4a1bf6c8",
+    "url": "/js/chunk-vendors.b724330f.js"
+  },
+  {
+    "revision": "f11bce428d38fd9f4d6f0ba31c2acffc",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
